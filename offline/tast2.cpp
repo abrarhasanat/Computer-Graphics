0ed8def5f702122b1f0d982853c6ea77a30441af
Mohammad Abrarul Hasanat <abrar.hasnat.haiyat@gmail.com>
2023-06-25 00:47:39 +0600

    one sphere is working now

--- offline/tast2.cpp
+++ offline/tast2.cpp
@@ -46,55 +46,117 @@ void drawAxes() {
 
 /* Draw a cube centered at the origin */
 void drawCube() {
-    glBegin(GL_QUADS);                // Begin drawing the color cube with 6 quads
-    // Top face (y = 1.0f)
-    // Define vertices in counter-clockwise (CCW) order with normal pointing out
-    glColor3f(0.0f, 1.0f, 0.0f);     // Green
-    glVertex3f(1.0f, 1.0f, -1.0f);
-    glVertex3f(-1.0f, 1.0f, -1.0f);
-    glVertex3f(-1.0f, 1.0f, 1.0f);
-    glVertex3f(1.0f, 1.0f, 1.0f);
-
-    // Bottom face (y = -1.0f)
-    glColor3f(1.0f, 0.5f, 0.0f);     // Orange
-    glVertex3f(1.0f, -1.0f, 1.0f);
-    glVertex3f(-1.0f, -1.0f, 1.0f);
-    glVertex3f(-1.0f, -1.0f, -1.0f);
-    glVertex3f(1.0f, -1.0f, -1.0f);
-
-    // Front face  (z = 1.0f)
-    glColor3f(1.0f, 0.0f, 0.0f);     // Red
-    glVertex3f(1.0f, 1.0f, 1.0f);
-    glVertex3f(-1.0f, 1.0f, 1.0f);
-    glVertex3f(-1.0f, -1.0f, 1.0f);
-    glVertex3f(1.0f, -1.0f, 1.0f);
-
-    // Back face (z = -1.0f)
-    glColor3f(1.0f, 1.0f, 0.0f);     // Yellow
-    glVertex3f(1.0f, -1.0f, -1.0f);
-    glVertex3f(-1.0f, -1.0f, -1.0f);
-    glVertex3f(-1.0f, 1.0f, -1.0f);
-    glVertex3f(1.0f, 1.0f, -1.0f);
-
-    // Left face (x = -1.0f)
-    glColor3f(0.0f, 0.0f, 1.0f);     // Blue
-    glVertex3f(-1.0f, 1.0f, 1.0f);
-    glVertex3f(-1.0f, 1.0f, -1.0f);
-    glVertex3f(-1.0f, -1.0f, -1.0f);
-    glVertex3f(-1.0f, -1.0f, 1.0f);
-
-    // Right face (x = 1.0f)
-    glColor3f(1.0f, 0.0f, 1.0f);     // Magenta
-    glVertex3f(1.0f, 1.0f, -1.0f);
-    glVertex3f(1.0f, 1.0f, 1.0f);
-    glVertex3f(1.0f, -1.0f, 1.0f);
-    glVertex3f(1.0f, -1.0f, -1.0f);
-    glEnd();  // End of drawing color-cube
 }
 
 double xheight = 1.0f;
 double radius = 0.01f;
 float scale = 0;
+
+
+
+std::vector<float> buildUnitPositiveX(int subdivision)
+{
+    const float DEG2RAD = acos(-1) / 180.0f;
+
+    std::vector<float> vertices;
+    float n1[3];        // normal of longitudinal plane rotating along Y-axis
+    float n2[3];        // normal of latitudinal plane rotating along Z-axis
+    float v[3];         // direction vector intersecting 2 planes, n1 x n2
+    float a1;           // longitudinal angle along Y-axis
+    float a2;           // latitudinal angle along Z-axis
+
+    // compute the number of vertices per row, 2^n + 1
+    int pointsPerRow = (int)pow(2, subdivision) + 1;
+
+    // rotate latitudinal plane from 45 to -45 degrees along Z-axis (top-to-bottom)
+    for (unsigned int i = 0; i < pointsPerRow; ++i)
+    {
+        // normal for latitudinal plane
+        // if latitude angle is 0, then normal vector of latitude plane is n2=(0,1,0)
+        // therefore, it is rotating (0,1,0) vector by latitude angle a2
+        a2 = DEG2RAD * (45.0f - 90.0f * i / (pointsPerRow - 1));
+        n2[0] = -sin(a2);
+        n2[1] = cos(a2);
+        n2[2] = 0;
+
+        // rotate longitudinal plane from -45 to 45 along Y-axis (left-to-right)
+        for (unsigned int j = 0; j < pointsPerRow; ++j)
+        {
+            // normal for longitudinal plane
+            // if longitude angle is 0, then normal vector of longitude is n1=(0,0,-1)
+            // therefore, it is rotating (0,0,-1) vector by longitude angle a1
+            a1 = DEG2RAD * (-45.0f + 90.0f * j / (pointsPerRow - 1));
+            n1[0] = -sin(a1);
+            n1[1] = 0;
+            n1[2] = -cos(a1);
+
+            // find direction vector of intersected line, n1 x n2
+            v[0] = n1[1] * n2[2] - n1[2] * n2[1];
+            v[1] = n1[2] * n2[0] - n1[0] * n2[2];
+            v[2] = n1[0] * n2[1] - n1[1] * n2[0];
+
+            // normalize direction vector
+            float scale = 1 / sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
+            v[0] *= scale;
+            v[1] *= scale;
+            v[2] *= scale;
+
+            // add a vertex into array
+            vertices.push_back(v[0]);
+            vertices.push_back(v[1]);
+            vertices.push_back(v[2]);
+        }
+    }
+    
+
+    return vertices;
+}
+
+void drawSphere() {
+
+    float vertices[] = { 1 - scale , 0 , 0 };
+    float radius = (1.0f * scale) / sqrt(3);
+    //radius = radius * 2.0f;
+    int subdivision = 10;
+    for (int i = 0; i < 1; ++i) {
+        glPushMatrix();
+        glTranslatef(vertices[0], vertices[1], vertices[2]);
+        vector<float> vertices = buildUnitPositiveX(subdivision);
+        
+        for (int i = 0; i < vertices.size(); i++) {
+          //  vertices[i] = vertices[i] * radius;
+        }
+        int pointsPerRow = (int)pow(2, subdivision) + 1;
+        for (unsigned int i = 0; i < pointsPerRow - 1; ++i)
+        {
+            glBegin(GL_TRIANGLE_STRIP);
+            for (unsigned int j = 0; j < pointsPerRow; ++j)
+            {
+                // vertex 1
+                // yellow color
+                glColor3f(1, 1, 0);
+                glNormal3f(vertices[3 * (i * pointsPerRow + j) + 0],
+                    vertices[3 * (i * pointsPerRow + j) + 1],
+                    vertices[3 * (i * pointsPerRow + j) + 2]);
+                glVertex3f(vertices[3 * (i * pointsPerRow + j) + 0] * radius,
+                    vertices[3 * (i * pointsPerRow + j) + 1] * radius,
+                    vertices[3 * (i * pointsPerRow + j) + 2] * radius);
+
+                // vertex 2
+                glNormal3f(vertices[3 * ((i + 1) * pointsPerRow + j) + 0],
+                    vertices[3 * ((i + 1) * pointsPerRow + j) + 1],
+                    vertices[3 * ((i + 1) * pointsPerRow + j) + 2]);
+                glVertex3f(vertices[3 * ((i + 1) * pointsPerRow + j) + 0] * radius,
+                    vertices[3 * ((i + 1) * pointsPerRow + j) + 1] * radius,
+                    vertices[3 * ((i + 1) * pointsPerRow + j) + 2] * radius);
+            }
+            glEnd();
+        }
+        glPopMatrix();
+    }
+
+}
+
 void drawCylinder() {
 
     float vertices[] = {
@@ -111,12 +173,74 @@ void drawCylinder() {
     int segments = 50;
     double tempx = radius, tempy = 0;
     double currx, curry;
-    for (int i = 0; i < 1; ++i) {
+    for (int i = 0; i < 12; ++i) {
         glPushMatrix();
-        glTranslatef( (1- scale) * 0.5f , (1- scale) * 0.5f , 0 );
+        glTranslatef((1 - scale) * 0.5f, (1 - scale) * 0.5f, 0);
+        // here begins the transloation 
+        if (i == 1) {
+            glTranslatef(-(1 - scale) * 0.5f * 2.0f, 0, 0);
+        }
+        if (i == 2) {
+            glTranslatef(-(1 - scale) * 0.5f, 0, -(1 -scale) * 0.5f) ;
+        }
+
+        if (i == 3) {
+            glTranslatef(-(1 - scale) * 0.5f, 0, (1 - scale) * 0.5f);
+        }
+
+        if (i == 4) {
+            glTranslatef(0, -(1 - scale) * 0.5f * 2.0f, 0);
+        }
+
+        if (i == 5) {
+            glTranslatef(-(1-scale) * 0.5f * 2.0f, - ( 1 - scale) * 0.5f * 2.0f , 0);
+        }
+        if (i == 6) {
+            glTranslatef(- (1-scale) * 0.5f, - ( 1 - scale) * 0.5f * 2.0f , (1-scale) * 0.5f);
+        }
+        if (i == 7) {
+            glTranslatef(-(1 - scale) * 0.5f, -(1 - scale) * 0.5f * 2.0f, -(1 - scale) * 0.5f);
+
+        }
+        if (i == 8) {
+            glTranslatef(0, -(1 - scale) * 0.5f, (1 - scale) * 0.5f);
+        }
+
+        if (i == 9) {
+            glTranslatef(0, -(1 - scale) * 0.5f, -(1 - scale) * 0.5f);
+        }
+        if (i == 10) {
+            glTranslatef(-(1 - scale) * 0.5f * 2.0f, -(1 - scale) * 0.5f, -(1 - scale) * 0.5f);
+        }
+
+        if (i == 11) {
+            glTranslatef(-(1 - scale) * 0.5f * 2.0f, -(1 - scale) * 0.5f, (1 - scale) * 0.5f);
+        }
+
         glRotatef(90, 1, 0, 0);
         glRotatef(45, 0, 1, 0);
-        if (i != 0) glRotatef(90 * i, 0, 1, 0);
+        if (i == 1) {
+            glRotatef(180, 1, 0, 1);
+        }
+        if (i == 2 or i == 6) {
+            glRotatef(-45, 0, 1, 0);
+            glRotatef(45, 1, 0, 0);
+        }
+        if (i == 3 or i == 7) {
+            glRotatef(-45, 0, 1, 0);
+            glRotatef(-45, 1, 0, 0);
+        }
+        if (i == 4) {
+            glRotatef(90, 0, 1, 0);
+        }
+        if (i == 8 or i == 10) {
+            glRotatef(45, 0, 1, 0);
+            glRotatef(45, 1, 0, 0);
+        }
+        if (i == 9 or i == 11) {
+            glRotatef(45, 0, 1, 0);
+            glRotatef(-45, 1, 0, 0);
+        }
         glBegin(GL_QUADS);
         for (int i = 1; i <= segments; i++) {
             double theta = i * 2.0 * M_PI / segments;
@@ -142,9 +266,7 @@ struct point {
     GLfloat x, y, z;
 };
 
-void drawSphere() {
-    
-}
+
 
 
 void drawOctahedron() {
@@ -217,7 +339,7 @@ void display() {
     if (isPyramid) drawPyramid();
     if (isOctahedron) drawOctahedron();
     drawCylinder();
-    //drawSphere();
+    drawSphere();
 
     glutSwapBuffers();  // Render now
 }
